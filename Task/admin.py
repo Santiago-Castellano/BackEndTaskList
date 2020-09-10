5507a8a95766745aaabaf54e7292eaf80a084079
Santaigo-Castellano <santiagocastellano01@gmail.com>
2020-09-09 22:21:45 -0300

    registra modelos

--- Task/admin.py
+++ Task/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from Task.models import GroupTask, TypeTask, Task
 # Register your models here.
+
+admin.site.register(GroupTask)
+admin.site.register(TypeTask)
+admin.site.register(Task)
