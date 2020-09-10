686dd4c4530e56eee0d550c35d8101dfc287ba74
Santaigo-Castellano <santiagocastellano01@gmail.com>
2020-09-10 18:53:57 -0300

    cambia urls

--- Task/urls.py
+++ Task/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 import Task.views.group_task as group_task
+import Task.views.type_task as type_task 
 
 
 urlpatterns = [
-    path('create_group_task/',group_task.create,name='create_group_task'),
-    path('update_group_task/<int:id>',group_task.update,name='update_group_task'),
-    path('detail_group_task/<int:id>',group_task.detail,name='detail_group_task'),
-    path('delete_group_task/<int:id>',group_task.delete,name='delete_group_task')
+    #Group Task
+    path('group/create/'        ,group_task.create,name='create_group_task'),
+    path('group/update/<int:id>',group_task.update,name='update_group_task'),
+    path('group/detail/<int:id>',group_task.detail,name='detail_group_task'),
+    path('group/delete/<int:id>',group_task.delete,name='delete_group_task'),
+    
+    #Type Task
+    path('type/create/',        type_task.create,name='create_type_task'),
+    path('type/update/<int:id>',type_task.update,name='update_type_task'),
+    path('type/detail/<int:id>',type_task.detail,name='detail_type_task'),
+    path('type/delete/<int:id>',type_task.delete,name='delete_type_task')
 ]
