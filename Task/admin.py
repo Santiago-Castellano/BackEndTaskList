69e097c2857dc217138356cffda0fcb2320e475f
Santaigo-Castellano <santiagocastellano01@gmail.com>
2020-09-10 17:36:23 -0300

    agrega modelo para compartir grupos

--- Task/admin.py
+++ Task/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from Task.models import GroupTask, TypeTask, Task
+from Task.models import GroupTask, TypeTask, Task,GroupShared
 # Register your models here.
 
 admin.site.register(GroupTask)
 admin.site.register(TypeTask)
 admin.site.register(Task)
+admin.site.register(GroupShared)
